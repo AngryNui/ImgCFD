b1ad81c1b3345b80bd39068d81d6c24aa57a7055
AngryNui <nui.liehr@gmail.com>
2020-12-04 22:07:59 +0100

    add existing files

---
+++ Assets/deforming_mesh/MeshDeformerInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour {
+	
+	public float force = 10f;
+	public float forceOffset = 0.1f;
+	Camera cam;
+	
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			HandleInput(0);
+		}
+		if (Input.GetMouseButton(1)) {
+			HandleInput(1);
+		}
+	}
+
+	void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+
+	void HandleInput (int mousebutton) {
+		Ray inputRay = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		if (Physics.Raycast(inputRay, out hit)) {
+			MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+			if (deformer) {
+				Vector3 point = hit.point;
+				if (mousebutton == 0) {
+					point += hit.normal * forceOffset;
+				} else {
+					point += hit.normal * forceOffset *-1;
+				}
+				deformer.AddDeformingForce(point, force);
+			}
+		}
+	}
+}
---
+++ Assets/shader_replacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shader_replacement : MonoBehaviour
+{
+
+	public Shader replacementShader;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        if (replacementShader != null){
+            GetComponent<Camera>().SetReplacementShader(replacementShader, "RenderType");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
